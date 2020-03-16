70125bc7c260d5d9e7ac1f4a8588f7ae29e7d642
quboka <1362420246@qq.com>
2020-03-16 12:09:05 +0800

    admin

--- boca-service/boca-admin/src/main/java/com/qbk/boca/admin/BocaAdminApplication.java
+++ boca-service/boca-admin/src/main/java/com/qbk/boca/admin/BocaAdminApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * EnableAdminServer 开启admin服务端
+ */
 @EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
