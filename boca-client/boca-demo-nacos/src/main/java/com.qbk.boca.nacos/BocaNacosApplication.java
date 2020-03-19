0d3c30b880f76f1e1781bc0abec06cc84254b6a0
quboka <1362420246@qq.com>
2020-03-19 20:58:53 +0800

    nacos配置

--- boca-client/boca-demo-nacos/src/main/java/com.qbk.boca.nacos/BocaNacosApplication.java
+++ boca-client/boca-demo-nacos/src/main/java/com.qbk.boca.nacos/BocaNacosApplication.java
@@ -3,9 +3,7 @@ package com.qbk.boca.nacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BocaNacosApplication {
