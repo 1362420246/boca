30d727bb657e1412a29e0cd5eb64b742e1ec0506
quboka <1362420246@qq.com>
2020-08-14 01:08:46 +0800

    dubbo负载均衡

--- boca-dubbo/boca-dubbo-springboot/dubbo-springboot-consumer/src/main/java/com/qbk/boca/dubbo/springboot/consumer/DubboSpringBootConsumerApplication.java
+++ boca-dubbo/boca-dubbo-springboot/dubbo-springboot-consumer/src/main/java/com/qbk/boca/dubbo/springboot/consumer/DubboSpringBootConsumerApplication.java
@@ -3,6 +3,8 @@ package com.qbk.boca.dubbo.springboot.consumer;
 import com.qbk.boca.dubbo.springboot.api.HelloService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.annotation.Reference;
+import org.apache.dubbo.rpc.cluster.loadbalance.ConsistentHashLoadBalance;
+import org.apache.dubbo.rpc.cluster.loadbalance.RoundRobinLoadBalance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +20,14 @@ public class DubboSpringBootConsumerApplication {
         SpringApplication.run(DubboSpringBootConsumerApplication.class,args);
     }
 
-    //多注册中心、版本控制
-    @DubboReference(registry = {"shanghai","beijing"},version = "2.0")
+    /**
+     * 用于引用Dubbo服务的注释
+     */
+    @DubboReference(
+            registry = {"shanghai","beijing"},//多注册中心
+            version = "2.0",//版本控制
+            loadbalance = RoundRobinLoadBalance.NAME //负载均衡
+    )
     private HelloService helloService;
 
     @GetMapping("/get")
