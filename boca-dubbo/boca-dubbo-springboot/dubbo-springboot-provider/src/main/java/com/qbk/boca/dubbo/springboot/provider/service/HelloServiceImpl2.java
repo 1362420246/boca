33f84327c64aa654bce4c405994bbe06b22b50c2
quboka <1362420246@qq.com>
2020-08-15 19:44:42 +0800

    dubbo容错策略

--- boca-dubbo/boca-dubbo-springboot/dubbo-springboot-provider/src/main/java/com/qbk/boca/dubbo/springboot/provider/service/HelloServiceImpl2.java
+++ boca-dubbo/boca-dubbo-springboot/dubbo-springboot-provider/src/main/java/com/qbk/boca/dubbo/springboot/provider/service/HelloServiceImpl2.java
@@ -3,6 +3,7 @@ package com.qbk.boca.dubbo.springboot.provider.service;
 
 import com.qbk.boca.dubbo.springboot.api.HelloService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.rpc.cluster.support.FailoverCluster;
 
 /**
  * 多注册中心、多协议、版本控制
@@ -10,7 +11,10 @@ import org.apache.dubbo.config.annotation.DubboService;
 @DubboService(
         registry = {"shanghai","beijing"},
         protocol = {"dubbo","rest"} ,
-        version = "2.0")
+        version = "2.0",
+        cluster = FailoverCluster.NAME , //容错策略 ， 2.7.5以后的版本不生效
+        retries = 5 //重试次数 ，默认两次。
+)
 public class HelloServiceImpl2 implements HelloService {
 
     @Override
